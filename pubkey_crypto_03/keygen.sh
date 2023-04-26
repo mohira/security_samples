3834482ec90dda931577335312422d9825a4ea24
mohira <shukatsudaison@gmail.com>
2023-04-26 12:19:02 +0900

    init

--- pubkey_crypto_03/keygen.sh
+++ pubkey_crypto_03/keygen.sh
@@ -1,4 +1,7 @@
 #!/bin/zsh
 
-openssl genrsa -out private_key.pem 2048
+openssl genrsa -out private_key.pem 4096
 openssl rsa -in private_key.pem -pubout -out public_key.pem
+
+# Ed25519
+# ssh-keygen -t ed25519 -C "your_email@example.com" -f ./ed25519_key
